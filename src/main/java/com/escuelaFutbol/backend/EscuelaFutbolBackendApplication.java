12b0479cd1bd55aa8a237d75cbba7539faa1b27b
J-benit <jhonbenitezvalderrama@gmail.com>
2025-05-30 10:22:57 -0500

    Primera versión del backend en Spring Boot con estructura MVC

---
+++ src/main/java/com/escuelaFutbol/backend/EscuelaFutbolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.escuelaFutbol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaFutbolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscuelaFutbolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escuelaFutbol/backend/EscuelaFutbolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuelaFutbol.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaFutbolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
